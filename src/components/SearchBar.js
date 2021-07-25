de252342646b28a627b1fd78971dc2bba3046e18
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-07-25 14:43:14 -0400

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import './SearchBar.css'
 const Searchbar = props => {
     const [term, setTerm] = useState('');
     return (
-        <div className="searchbar">
+        <div className="searchbar navbar-rnd">
             <input placeholder="Where would you like to go?"
             value={term} onChange={e => setTerm(e.target.value)}></input>
         </div>
