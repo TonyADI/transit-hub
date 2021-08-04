efff63f69b822bf73bd4d2c774d7d2d3e6b54c50
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-08-03 20:44:33 -0400

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+const Footer = props => {
+    return (
+        <footer className="footer">
+          © Copyright TonyADI. All Rights Reserved.
+          v1.0
+        </footer>
+    )
+}
+
+export default Footer;
