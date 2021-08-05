0b434a6953d16cca31af5e1a63c00e6bcf7a24cd
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-08-04 22:50:25 -0400

    Increase number of returned images to 4

--- src/utils/UnsplashAPI.js
+++ src/utils/UnsplashAPI.js
@@ -1,7 +1,7 @@
 const client_id = 'T0jCXzIigIBjuwNApWDyTtpNs_xUi2Lu1hWOBMjKFFs';
 
 const photoSearch = async term => {
-    const response = await fetch(`https://api.unsplash.com/search/photos?per_page=3&page=1&query=${term}&orientation=landscape&client_id=${client_id}`);
+    const response = await fetch(`https://api.unsplash.com/search/photos?per_page=4&page=1&query=${term}&orientation=landscape&client_id=${client_id}`);
     const jsonResponse = await response.json();
     return jsonResponse.results ? jsonResponse.results.map(result => {
         return {imgUrl: result.urls.full,
