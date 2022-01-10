b3a5bd14b75bd9eb56f420f5df4534a1514a5d37
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-01-10 09:58:55 -0500

    remove credentials

--- src/utils/UnsplashAPI.js
+++ src/utils/UnsplashAPI.js
@@ -1,14 +1,22 @@
 const client_id = '';
 
 const photoSearch = async term => {
-    const response = await fetch(`https://api.unsplash.com/search/photos?per_page=4&page=1&query=${term}&orientation=landscape&client_id=${client_id}`);
-    const jsonResponse = await response.json();
-    return jsonResponse.results ? jsonResponse.results.map(result => {
-        return {imgUrl: result.urls.full,
-                id: result.id,
-                descrp: result.description}
-                }) :
-     [];
+    try {
+        const response = await fetch(`https://api.unsplash.com/search/photos?per_page=4&page=1&query=${term}&orientation=landscape&client_id=${client_id}`);
+        if(response.ok){
+            const jsonResponse = await response.json();
+            return jsonResponse.results ? jsonResponse.results.map(result => {
+                return {imgUrl: result.urls.regular,
+                        id: result.id,
+                        descrp: result.description}
+                        }) : [];
+        }
+        return [];
+    }
+    catch(error) {
+        console.log(error);
+        return [];
+    }
 }
 
 export default photoSearch;
--- src/utils/YelpAPI.js
+++ src/utils/YelpAPI.js
@@ -3,25 +3,29 @@ const businessSearch = async (term, location) => {
     try {
         const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&limit=8&location=${location}`,
             { headers: { Authorization: `Bearer ${apiKey}` } });
-        const jsonResponse = await response.json();
-        return jsonResponse.businesses ? jsonResponse.businesses.map(business => {
-            return {
-                id: business.id,
-                imageSrc: business.image_url,
-                name: business.name,
-                address: business.location.address1,
-                city: business.location.city,
-                state: business.location.state,
-                zipCode: business.location.zipCode,
-                category: business.categories[0].title,
-                price: business.price,
-                rating: business.rating,
-                reviewCount: business.review_count,
-                url: business.url
-            };
-        }) : null;
+        if(response.ok){
+            const jsonResponse = await response.json();
+            return jsonResponse.businesses ? jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zipCode,
+                    category: business.categories[0].title,
+                    price: business.price,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                    url: business.url
+                };
+            }) : [];
+        }
+        return [];
     } catch (error) {
-        return console.log(error);
+        console.log(error);
+        return [];
     }
 }
 
