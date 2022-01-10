25bed13c549fb0cf4adb1298fa870748997b7bc8
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-01-10 09:59:25 -0500

    Update Covid19API.js

--- src/utils/Covid19API.js
+++ src/utils/Covid19API.js
@@ -2,12 +2,16 @@ const covidSearch = async country => {
     try {
         let hyphenCountry = country.replace(' ', '-');
         const response = await fetch(`https://api.covid19api.com/total/country/${hyphenCountry}`);
-        const jsonResponse = await response.json();
-        return Array.isArray(jsonResponse) && jsonResponse.pop() ? jsonResponse.pop() :
-        {};
+        if(response.ok){
+            const jsonResponse = await response.json();
+            return Array.isArray(jsonResponse) && jsonResponse.pop() ? jsonResponse.pop() :
+            {};
+        }
+        return {};
     }
     catch(error){
         console.log(error);
+        return {};
     }
 }
 
