b113305d4ae5cac29d76ea45377456f5245db07d
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-01-10 09:57:21 -0500

    Update BusinessList.js

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -3,14 +3,22 @@ import './BusinessList.css';
 
 const BusinessList = props => {
     return (
-        <div className="businessList">
-            {!props.loading ? props.businesses ? 
-            props.businesses.map(business => 
-            { return <Business key={business.id} business={business}/>}) : 
-            <div>This service may not be available at this location. Try a different location.</div>
-            :<div className="loading">
-                <div></div><div></div><div></div>
+        <div>
+            <h2>{props.heading || 'Businesses'}</h2>
+            <div className="businessList">
+                {!props.loading ? props.businesses.length ? props.businesses.map(business => 
+                { return <Business key={business.id} address={business.address}
+                city={business.city} category={business.category} imageSrc={business.imageSrc}
+                name={business.name} price={business.price} state={business.state}
+                rating={business.rating} review={business.review} url={business.url}
+                zipcode={business.zipcode}/>}) : 
+                <div>This service may not be available at this location. Try a different location.</div>
+                :<div className="loading">
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>}
+            </div>
         </div>
 
     );
