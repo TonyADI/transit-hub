2331cb92017098f2b16fa4d596c59a413ff0949a
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-01-22 18:13:59 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,17 @@ const Navbar = props => {
         <div className="navbar-container">
             <nav className="navbar body-padding">
                 <div className="logo-link-container">
-                    <a className="logo-link" href="./">Transit Hub</a></div>
-                <SearchBar term={props.country} handleChange={props.handleChange}
-                handleClick={props.handleClick}/>
+                    <a className="logo-link" href="./">Transit Hub</a>
+                </div>
+                <SearchBar 
+                    term={props.country} 
+                    handleChange={props.handleChange}
+                    handleClick={props.handleClick}
+                    validCountry={props.validCountry}
+                />
                 <div className="login-bt-container">
-                    <button className="login-bt navbar-rnd">Login</button></div>
+                    <button className="login-bt">Login</button>
+                </div>
             </nav>
         </div>
     )
