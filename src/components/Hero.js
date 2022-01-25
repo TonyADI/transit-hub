6584b523107f2057737a1b0fbf09334562a8eaa3
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-01-25 14:15:37 -0500

    remove button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,18 @@
 const Hero = props => {
     return (
-        <div className="hero-container">
+        <div className="hero-container" style={{backgroundImage: `url(${props.backgroundImage})`}}>
             <div className="img-overlay">
                 <div className="hero-body">
                     <span className="hero-heading">
-                    One-stop shop for all your travel needs</span>
-                    <a id="hero-link" href='#destination'>
-                        <button className="hero-bt" onClick={props.handleClick}>
-                            Take me away
-                        </button>
+                        One-stop shop for all your travel needs
+                    </span>
+                    <a  
+                        id="hero-link" 
+                        href='#destination' 
+                        className="hero-bt" 
+                        onClick={props.handleClick}
+                    >
+                        Take me away
                     </a>
                 </div>
             </div>
